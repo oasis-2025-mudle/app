ea4f9107fff16264e3900d517ee387f6315030d7
RosezNU <zhao.ruoyu@northeastern.edu>
2025-02-02 13:49:48 -0500

    Change reactor

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={Footer} />
+        <Route path='about' element={Footer} />
+        <Route path='home' element={Footer} />
+        <Route path='asfdsaf' element={Footer} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
